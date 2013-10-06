47fb2af559faebc5b8b981e3fbbc803cdaf8c432
RexWalker <cangelbeats@gmail.com>
2013-10-07 02:11:03 +0800

    add the config

---
+++ src/com/editor/EditorActivity.java
@@ -0,0 +1,98 @@
+package com.editor;
+
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.graphics.PixelFormat;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+
+import android.widget.TextView;
+import roboguice.activity.RoboActivity;
+import roboguice.inject.InjectView;
+
+public class EditorActivity extends RoboActivity {
+	private MySurfaceView mGLSurfaceView;
+
+    LinearLayout imageButtonLinearLayout;
+    @InjectView(R.id.textView2)  TextView sUp;
+
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+
+        //设置为全屏
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN ,
+		              WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		//设置为横屏模式
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        setContentView(R.layout.main);
+
+
+    /* -------------------- Start add ImageButtons------------------------------------- */
+        Context context = this.getApplicationContext();
+        imageButtonLinearLayout = new LinearLayout(this);
+        imageButtonLinearLayout.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.FILL_PARENT, TableLayout.LayoutParams.FILL_PARENT));
+        imageButtonLinearLayout.setOrientation(LinearLayout.VERTICAL);
+        // add imagebuttons
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        LinearLayout imagebuttonLinearLayout = (LinearLayout) inflater.inflate(R.layout.imagebutton, imageButtonLinearLayout, false);
+        imageButtonLinearLayout.addView(imagebuttonLinearLayout);
+    /* -------------------- End add ImageButtons------------------------------------- */
+
+
+
+        //初始化GLSurfaceView
+        mGLSurfaceView = new MySurfaceView(this);
+
+        mGLSurfaceView.requestFocus();//获取焦点
+        mGLSurfaceView.setFocusableInTouchMode(true);//设置为可触控
+
+//        mGLSurfaceView.setZOrderOnTop(true);
+        mGLSurfaceView.getHolder().setFormat(PixelFormat.TRANSPARENT);
+        setContentView(mGLSurfaceView);
+
+
+        // 添加imageButtonLinearLayout (SurfaceView)
+        addContentView(imageButtonLinearLayout, new TableLayout.LayoutParams(TableLayout.LayoutParams.FILL_PARENT,
+                TableLayout.LayoutParams.FILL_PARENT));
+
+//         sUp.setOnClickListener(new View.OnClickListener() {
+//             @Override
+//             public void onClick(View view) {
+//                 //To change body of implemented methods use File | Settings | File Templates.
+//
+//                  mGLSurfaceView.setUp();
+//             }
+//         });
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGLSurfaceView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mGLSurfaceView.onPause();
+    }    
+}
+
+
+
