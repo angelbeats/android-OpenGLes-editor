234e1f2e18f9ec5ca3adf5c7049910b6a415c1d7
angelbeats <cangelbeats@gmail.com>
2013-10-10 00:04:10 +0800

    delete useless word

--- src/com/editor/EditorActivity.java
+++ src/com/editor/EditorActivity.java
@@ -1,4 +1,5 @@
 package com.editor;
+
 import android.content.pm.ActivityInfo;
 import android.graphics.PixelFormat;
 import android.os.Bundle;
@@ -8,75 +9,35 @@ import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.Toast;
 import com.editor.common.ArcMenu.ArcMenu;
-import com.editor.common.ArcMenu.RayMenu;
 import roboguice.activity.RoboActivity;
+import roboguice.inject.InjectView;
 
 public class EditorActivity extends RoboActivity {
-    private EditorGLSurfaceView _editorView;
-
     private static final int[] ITEM_DRAWABLES = { R.drawable.composer_camera, R.drawable.composer_music,
             R.drawable.composer_place, R.drawable.composer_sleep, R.drawable.composer_thought, R.drawable.composer_with };
 
-
+    @InjectView(R.id.arc_menu) ArcMenu _arcMenu;
+    @InjectView(R.id.editorGLSurfaceView) EditorGLSurfaceView _editorView;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-
         setContentView(R.layout.main);
-
-
-//        _editorView = new EditorGLSurfaceView(this);
-
-        _editorView = (EditorGLSurfaceView)findViewById(R.id.editorGLSurfaceView);
-
         _editorView.requestFocus();
         _editorView.setFocusableInTouchMode(true);
-
         _editorView.getHolder().setFormat(PixelFormat.TRANSPARENT);
-//        setContentView(_editorView);
-
-
-
-
-        ArcMenu arcMenu  = (ArcMenu) this.findViewById(R.id.arc_menu);
-
-//        ArcMenu arcMenu2 = (ArcMenu) this.findViewById(R.id.arc_menu_2);
-//        RayMenu rayMenu  = (RayMenu) this.findViewById(R.id.ray_menu);
-        initArcMenu(arcMenu, ITEM_DRAWABLES);
-
-//        initArcMenu(arcMenu2, ITEM_DRAWABLES);
-//        final int itemCount = ITEM_DRAWABLES.length;
-//        for (int i = 0; i < itemCount; i++) {
-//            ImageView item = new ImageView(this);
-//            item.setImageResource(ITEM_DRAWABLES[i]);
-//
-//            final int position = i;
-//            arcMenu.addItem(item, new View.OnClickListener() {
-//
-//                @Override
-//                public void onClick(View v) {
-//                    Toast.makeText(EditorActivity.this, "position:" + position, Toast.LENGTH_SHORT).show();
-//                }
-//            });// Add a menu item
-//        }
-
+        initArcMenu(_arcMenu, ITEM_DRAWABLES);
     }
-
     private void initArcMenu(ArcMenu menu, int[] itemDrawables) {
         final int itemCount = itemDrawables.length;
         for (int i = 0; i < itemCount; i++) {
             ImageView item = new ImageView(this);
             item.setImageResource(itemDrawables[i]);
-
             final int position = i;
             menu.addItem(item, new View.OnClickListener() {
-
                 @Override
                 public void onClick(View v) {
                     Toast.makeText(EditorActivity.this, "position:" + position, Toast.LENGTH_SHORT).show();
@@ -84,13 +45,11 @@ public class EditorActivity extends RoboActivity {
             });
         }
     }
-
     @Override
     protected void onResume() {
         super.onResume();
         _editorView.onResume();
     }
-
     @Override
     protected void onPause() {
         super.onPause();
